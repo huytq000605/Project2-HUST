8c2a1e4aecc25313e3065fdd6f742d92bb263b10
huytq <ygoldendarkness@gmail.com>
2021-12-25 15:57:03 +0700

    Setup Knex & MySQL

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-import express from 'express'
-import mqtt from './mqtt.js'
+import express from 'express';
+import mqtt from './mqtt.js';
+import mysql from 'mysql';
+import client from 'knex';
 
 const app = express()
 const port = 3000
+
+const knex = client({
+  client: 'mysql',
+  connection: {
+    host : '127.0.0.1',
+    port : 3306,
+    user : 'root',
+    password : 'secret',
+    database : 'project2'
+}
+})
+
+// Write migration
+console.log(await knex("tanmatmac").select("1+1"))
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
